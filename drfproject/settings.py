a0eec0b8146e449c6eed65030639984777aba62e
AlexeyGarmash <i.republic2014@gmail.com>
2020-01-20 13:17:43 +0000

    change allowed host

--- drfproject/settings.py
+++ drfproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'blf@l!xixhcnscl(i!2sfq1kx7tpj+iv1xj$a6itd%nuhy=2ep'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alexeygarmash.pythonanywhere.com']
 
 
 # Application definition
@@ -139,5 +139,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    'static'
+]
 MEDIA_URL = 'media/'
